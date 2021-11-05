d8a57cc4397617ddfe1eb095bd0ebb437e00acfe
Emmanuel Lopez <jesusemm010@hotmail.com>
2021-11-04 22:35:31 -0600

    Componente para el formulario ya con estilos

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React, {Fragment} from 'react'
+
+// function Header({title}){
+
+//     console.log(title)
+//     return(
+        
+//         <Fragment>
+//             <h1>{title}</h1>
+//         </Fragment>
+//     );
+// }
+const Header = ({title}) => (
+        <Fragment>
+            <h1>{title}</h1>
+        </Fragment>
+)
+
+
+ 
+export default Header
